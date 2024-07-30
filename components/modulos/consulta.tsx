01a3eda662959b345cc83864f1fd0520535c18c1
Thais-DN <142531680+Thais-DN@users.noreply.github.com>
2024-07-30 17:36:33 +0100

    add modificações

--- components/modulos/consulta.tsx
+++ components/modulos/consulta.tsx
@@ -1,5 +1,19 @@
-export default function Consulta(){
+
+interface Props {
+    handleBeforeStep: () => void;
+    handleNextStep: () => void;
+}
+
+
+export default function Consulta({handleBeforeStep, handleNextStep}:Props){
     return (
-        <div>Consulta</div>
+        <div>
+            Consulta
+
+            <div className="flex mt-4">
+                <button onClick={handleBeforeStep} className="mr-2 p-2 bg-gray-200 rounded">Previous</button>
+                <button onClick={handleNextStep} className="ml-2 p-2 bg-gray-200 rounded">Next</button>
+            </div>
+        </div>
     )
 }
--- components/modulos/geral.tsx
+++ components/modulos/geral.tsx
@@ -1,5 +1,18 @@
-export default function Geral(){
+
+interface Props {
+    handleBeforeStep: () => void;
+    handleNextStep: () => void;
+}
+
+
+export default function Geral({handleBeforeStep, handleNextStep}:Props){
     return (
-        <div>Geral</div>
+    <div>
+        Geral
+        <div className="flex mt-4">
+            <button onClick={handleBeforeStep} className="mr-2 p-2 bg-gray-200 rounded">Previous</button>
+            <button onClick={handleNextStep} className="ml-2 p-2 bg-gray-200 rounded">Enviar</button>
+        </div>
+    </div>
     )
 }
